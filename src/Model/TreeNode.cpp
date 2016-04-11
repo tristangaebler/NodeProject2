4bb13ebfc71abc7a10cacf07b0491b66b2ef644b
tristangaebler <Tristan.Gaebler@gmail.com>
2016-04-11 08:36:14 -0600

    All TreeNode contructors finsihed

--- src/Model/TreeNode.cpp
+++ src/Model/TreeNode.cpp
@@ -15,3 +15,22 @@ TreeNode<Type> :: TreeNode() : Node<Type>()
     this->rightChild = nullptr;
     this->parent = nullptr;
 }
+
+template <class Type>
+TreeNode<Type> :: TreeNode(const Type & value) : Node<Type>(value)
+{
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+    this->parent = nullptr;
+    this->setValue(value);
+}
+
+template <class Type>
+TreeNode<Type> :: TreeNode(const Type & value, TreeNode<Type> * parent) : Node<Type>(value)
+{
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+    this->parent = nullptr;
+    this->setValue(value);
+    this->setParent(parent);
+}
