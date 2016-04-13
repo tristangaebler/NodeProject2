4aa39d56340766c2427a6fe6201752cde4334b6f
tristangaebler <Tristan.Gaebler@gmail.com>
2016-04-13 09:53:36 -0600

    Single insert method added

--- src/Model/CTECBinaryTree.cpp
+++ src/Model/CTECBinaryTree.cpp
@@ -11,6 +11,15 @@
 using namespace std;
 
 template <class Type>
+CTECBinaryTree<Type> :: CTECBinaryTree()
+{
+    root = nullptr;
+    height = 0;
+    balanced = true;
+    size = 0;
+}
+
+template <class Type>
 void CTECBinaryTree<Type> :: inorderTraversal(TreeNode<Type> * currentNode)
 {
     if(currentNode != nullptr)
@@ -65,6 +74,82 @@ void CTECBinaryTree<Type> :: calculateSize(TreeNode<Type> * currentNode)
         size++;
     }
 }
+
+template <class Type>
+bool CTECBinaryTree<Type> :: contains(Type value)
+{
+    bool isInTree = false;
+    if (root->getValue() == value)
+    {
+        return true;
+    }
+    else if (value < root->getValue())
+    {
+        return contains(value, root->getLeftChild());
+    }
+    else
+    {
+        isInTree =  contains(value, root->getRightChild());
+    }
+    
+    return isInTree;
+}
+
+template <class Type>
+bool CTECBinaryTree<Type> :: contains(Type value, CTECBinaryTree<Type> * currentTree)
+{
+    if(currentTree == nullptr)
+    {
+        return false;
+    }
+    if (currentTree->getRoot()->getValue() == value)
+    {
+        return true;
+    }
+    else if (value < currentTree->getRoot()->root->getValue())
+    {
+        return contains(value, currentTree->getRoot()->root->getLeftChild());
+    }
+    else
+    {
+        return contains(value, currentTree->getRoot()->root->getRightChild());
+    }
+    
+    return false;
+}
+
+template <class Type>
+bool CTECBinaryTree<Type> :: insert(const Type &value)
+{
+    if (contains(value))
+    {
+        return false;
+    }
+    else
+    {
+        if(value < root->getValue())
+        {
+            
+        }
+        else
+        {
+            
+        }
+        
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
