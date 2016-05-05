24cea6866225c96c7024990c30997bfdfd9263f3
tristangaebler <Tristan.Gaebler@gmail.com>
2016-05-05 08:39:58 -0600

    Commith the headith

--- src/Model/HashTable.hpp
+++ src/Model/HashTable.hpp
@@ -10,5 +10,27 @@
 #define HashTable_hpp
 
 #include <stdio.h>
+namespace CTECData
+{
+    template <class Type>
+    class HashTable
+    {
+    private:
+        int capacity;
+        double efficiencyPercentage;
+        int size;
+        Type * internalStorage;
+        
+        int findPosition(const Type& value);
+        int handleCollision(const Type& value);
+        void updateSize();
+    public:
+        HashTable();
+        ~HashTable();
+        void add(const Type& value);
+        bool remove(Type value);
+        bool contains(const Type& value); 
+    };
+}
 
 #endif /* HashTable_hpp */
